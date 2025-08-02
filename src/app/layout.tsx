02780c8c3a096143cb1e4356166df3e54d74dcb8
Midun Thangavel <midunthangavel@gmail.com>
2025-08-02 16:44:34 +0000

    No, it's not working; better remove the part that we changed to load fas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,28 +3,11 @@
 
 import "./globals.css";
 import { Header } from "@/components/layout/header";
-import dynamic from 'next/dynamic';
+import { Footer } from "@/components/layout/footer";
 import { Toaster } from "@/components/ui/toaster";
 import { usePathname } from "next/navigation";
 import { ThemeProvider } from "next-themes";
-import { PT_Sans, Playfair_Display } from 'next/font/google';
 import { cn } from "@/lib/utils";
-
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  variable: '--font-headline',
-  display: 'swap',
-});
-
-const ptSans = PT_Sans({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-body',
-  display: 'swap',
-});
-
-
-const Footer = dynamic(() => import('@/components/layout/footer').then(mod => mod.Footer), { ssr: false });
 
 export default function RootLayout({
   children,
@@ -36,8 +19,11 @@ export default function RootLayout({
       <head>
         <title>FixmyEvent</title>
         <meta name="description" content="Find and book venues and services for your events." />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400..900&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn("font-body antialiased", ptSans.variable, playfair.variable)}>
+      <body className={cn("font-body antialiased")}>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
