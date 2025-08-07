99c54a9a5560a2d8775ea06831d7f68f7f98ec6d
Midun Thangavel <midunthangavel@gmail.com>
2025-08-07 09:29:56 +0000

    My users are mostly mobile users, so design accordingly. And Google Sign

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -38,7 +38,7 @@ export default function AppLayout({
             <FavoritesProvider>
                 <div className="relative flex min-h-screen flex-col bg-background">
                   <Header />
-                  <main className="flex-1 pb-16 md:pb-0">{children}</main>
+                  <main className="flex-1 pb-20">{children}</main>
                   <Footer />
                 </div>
               <Toaster />
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   "projectId": "venuevoyager-kga4a",
   "appId": "1:70267555311:web:fbd31ed89884df503afa1a",
@@ -17,6 +14,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
