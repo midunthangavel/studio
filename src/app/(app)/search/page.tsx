a87cf2b73650f9fdb2956699ced6a52a5f53d40a
Midun Thangavel <midunthangavel@gmail.com>
2025-08-08 12:38:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/search/page.tsx
+++ src/app/(app)/search/page.tsx
@@ -1,11 +1,13 @@
 
+'use client';
 import { allVenues } from '@/lib/venues';
 import { SearchPageClient } from './page.client';
 import type { VenueCardProps } from '@/components/venue-card';
+import { use } from 'react';
+
 
 // This is a Server Component
 export default function SearchPage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
-  
   const query = searchParams?.q as string | undefined;
 
   let searchResults: (VenueCardProps & { category: string; })[] = [];
